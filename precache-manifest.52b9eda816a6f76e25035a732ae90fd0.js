1d6546bea864fffbaec843a6de8b423cc40b00d7
mehedi23 <satusatusatu0.com>
2020-09-08 12:31:01 +0600

    Updates

---
+++ precache-manifest.52b9eda816a6f76e25035a732ae90fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4645155ca0475176cee16476f8c88c6",
+    "url": "/react-form_validation-api/index.html"
+  },
+  {
+    "revision": "71c05c77f5389b8b63bb",
+    "url": "/react-form_validation-api/static/css/main.fd442da6.chunk.css"
+  },
+  {
+    "revision": "835bf143a557bddbdf09",
+    "url": "/react-form_validation-api/static/js/2.150b3af0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-form_validation-api/static/js/2.150b3af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c05c77f5389b8b63bb",
+    "url": "/react-form_validation-api/static/js/main.dcf783d1.chunk.js"
+  },
+  {
+    "revision": "b1b01a62aff205e96e78",
+    "url": "/react-form_validation-api/static/js/runtime-main.5c69a754.js"
+  }
+]);
